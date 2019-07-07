8ce4964e982b2437eb0f77928e06eb3f0e046bdc
wbf17739094074 <17739094074@163.com>
2019-07-07 12:20:08 +0800

    Initial Commit

---
+++ ffmpeg播放器/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ffmpeg播放器
+//
+//  Created by wu baofeng on 2019/7/7.
+//  Copyright © 2019 wu baofeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
